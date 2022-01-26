aefeab8dc5aae4b52a37e92d78bcc86635b4bfbc
gnb2000 <barirodriguez@gmail.com>
2022-01-26 10:01:50 -0300

    First commit

---
+++ src/main/java/com/javaFullStackEldar/JavaFullStackEldar/JavaFullStackEldarApplication.java
@@ -0,0 +1,13 @@
+package com.javaFullStackEldar.JavaFullStackEldar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFullStackEldarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFullStackEldarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaFullStackEldar/JavaFullStackEldar/JavaFullStackEldarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaFullStackEldar.JavaFullStackEldar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFullStackEldarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
